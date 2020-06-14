9d7edac4e834ba902e7d0db2aeb35d9959efc455
yezid00 <olanaseyezid@gmail.com>
2020-06-14 21:37:52 +0100

    reroute the login view to index page

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::delete('projects/{id}/task','TaskController@destroy');
 //Route::resource('projects','TaskController');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
